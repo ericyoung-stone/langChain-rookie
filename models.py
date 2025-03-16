b312979279bd468eeda47a5d3e918b05cce7f006
Eric Young <bai603984772@163.com>
2025-03-16 11:16:45 +0800

    [how-to guides]base_url to ChatOpenAI && 01-How to return structured data from a model

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+from langchain_openai import ChatOpenAI
 from openai import OpenAI
 import os
 
@@ -43,3 +44,36 @@ class MyOpenAIModel:
             return result
 
         return response.choices[0].message.content
+
+
+
+"""
+# ⭐️ChatOpenAI 使用自己的base_url,使本身不支持tool的模型,支持:
+- 支持结构化输出, with_structured_output方法
+- 支持使用工具, bind_tools方法
+llm = ChatOpenAI(
+                model="xxx",
+                temperature=0,
+                max_tokens=None,
+                timeout=None,
+                max_retries=2,
+                # api_key="...",
+                # base_url="...",
+                # organization="...",
+                # other params...
+            )
+"""
+def get_base_url_model_with_tools(base_url: str="http://192.168.1.22:1234/v1", model: str="gemma-3-4b-it") -> ChatOpenAI:
+    llm = ChatOpenAI(
+        request_timeout=None,
+        openai_api_base=base_url,
+        model_name=model,
+        openai_api_key="custom",
+        max_retries=3,
+        max_tokens=8064,
+        temperature=0.7,
+    )
+    # openai_api_base 就是llama-server 部署时监听的地址
+    # openai_api_key 必须要填 随便填就行 不能为 ""
+    # print(llm.invoke("你是谁?").content)
+    return llm
